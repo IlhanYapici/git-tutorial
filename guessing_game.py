6b15d22e3045f6ba475ceff49a733272710dd576
Ilhan YAPICI <ilhan.yapici@edu.devinci.fr>
2020-12-18 10:55:39 +0100

    Add a guessing game

---
+++ guessing_game.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+import random
+
+
+# Defin the game in a function
+
+def guess_loop():
+	# This is the number the user will have to guess, chosen randomly in between 1 and 100
+	print("Whats's your name ?")
+	name = str (input())
+	number_to_guess = random.randint(1, 100)
+	print("I have in mind a number in between 1 and 100, can you find it?")
+
+
+	# Replay the question until the usesr finds the correct number
+	while True:
+		try:
+			# Read the number the user imputs
+			guess = int (input())
+
+			# Compare it to the number to guess
+			if guess > number_to_guess:
+				print("The number to guess is lower")
+			elif guess < number_to_guess:
+				print("The number to guess is higher")
+			else:
+				# The user found the number to guess, let's exit
+				print("Congrats ", name, "! You just found the number, it was indeed", guess)
+				return
+		# A ValueError is raised by the int() function if the user inputs something else than a number
+		except ValueError as err:
+			print ("Invalid input, please enter an integer")
+# Launch the game
+guess_loop()
